c0fd23f73732045d357186f7dcdf6635a4ce4fd8
Lucas Wiman <lucaswiman@counsyl.com>
2016-04-15 18:35:33 -0700

    Initial commit for boolean expressions and cnf

---
+++ pyreasoner/expressions/__init__.py
@@ -0,0 +1,186 @@
+from __future__ import division, absolute_import, unicode_literals
+
+import abc
+from functools import reduce
+from itertools import chain
+import re
+import operator
+
+from six import with_metaclass
+
+class Evaluable(with_metaclass(abc.ABCMeta)):
+    @abc.abstractmethod
+    def eval(self, namespace):
+        pass
+
+
+def eval_expr(expr, namespace):
+    if isinstance(expr, Evaluable):
+        return expr.eval(namespace)
+    else:
+        return expr
+
+def variables(names):
+    if not isinstance(input, (list, tuple)):
+        names = re.split(r'[, ]', names)
+    return map(Var, names)
+
+
+def is_boolean_atom(obj):
+    return isinstance(obj, (Not, Var, bool))
+
+
+def is_disjunction_of_atoms(expr):
+    return is_boolean_atom(expr) or (
+        isinstance(expr, Or) and
+        all(is_boolean_atom(child) for child in expr.children))
+
+
+def is_conjunctive_normal_form(expr):
+    if is_boolean_atom(expr):
+        return True
+    return is_disjunction_of_atoms(expr) or (
+        isinstance(expr, And) and
+        all(is_disjunction_of_atoms(child) for child in expr.children)
+    )
+
+
+def convert_to_conjunctive_normal_form(expr):
+    if is_disjunction_of_atoms(expr):
+        return expr
+    elif isinstance(expr, Not):
+        return convert_to_conjunctive_normal_form(expr.distribute_inwards())
+    elif isinstance(expr, Or):
+        if len(expr.children) == 1:
+            return expr.children[0]
+        split = len(expr.children // 2)
+        left, right = expr.children[:split], expr.children[split:]
+        return convert_to_conjunctive_normal_form(
+            And(*(Not(child).distribute_inwards() for child in left))
+            & And(*(Not(child).distribute_inwards() for child in right))
+        )
+    elif isinstance(expr, And):
+        return reduce(
+            operator.and_,
+            (convert_to_conjunctive_normal_form(child) for child in expr.children))
+    else:
+        return expr
+
+class ExpressionNode(object):
+    def __or__(self, other):
+        return Or(self, other)
+
+    def __and__(self, other):
+        return And(self, other)
+
+    def __invert__(self):
+        return Not(self)
+
+
+class Var(ExpressionNode):
+    __slots__ = ('name', )
+
+    def __init__(self, name=None):
+        if name is None:
+            name = 'x_%s' % id(self)
+        self.name = name
+
+    def eval(self, namespace):
+        if self in namespace:
+            return namespace[self]
+        elif self.name in namespace:
+            return namespace[self.name]
+        return self
+
+    def __str__(self):
+        return self.name
+
+    __repr__ = __str__
+
+    def __hash__(self):
+        return hash(self.name)
+
+    def __eq__(self, other):
+        return self.name == other.name
+
+
+class BooleanOperation(ExpressionNode):
+    __slots__ = ('children', )
+
+    def __init__(self, *children):
+        self.children = children
+
+    def eval(self, namespace):
+        evaluated = [eval_expr(child, namespace) for child in self.children]
+        if hasattr(self, 'default'):
+            return reduce(self.operator, evaluated, self.default)
+        return reduce(self.operator, evaluated)
+
+    def __eq__(self, other):
+        return type(self) == type(other) and self.children == other.children
+
+
+class Or(BooleanOperation):
+    operator = operator.or_
+
+    def __str__(self):
+        return '(%s)' % ' | '.join(str(child) for child in self.children)
+
+    __repr__ = __str__
+
+    def __or__(self, other):
+        if isinstance(other, Or):
+            return Or(*chain(self.children, other.children))
+        else:
+            return Or(*chain(self.children, [other]))
+
+
+class And(BooleanOperation):
+    operator = operator.and_
+
+    def __str__(self):
+        return '(%s)' % ' & '.join(str(child) for child in self.children)
+
+    __repr__ = __str__
+
+    def __and__(self, other):
+        if isinstance(other, And):
+            return And(*chain(self.children, other.children))
+        else:
+            return And(*chain(self.children, [other]))
+
+
+class Not(BooleanOperation):
+    def __init__(self, child):
+        self.children = (child, )
+
+    def eval(self, namespace):
+        evaluated = eval_expr(self.children[0], namespace)
+        if isinstance(evaluated, ExpressionNode):
+            return ~evaluated
+        elif isinstance(evaluated, bool):
+            # Boolean
+            return not evaluated
+        else:
+            raise TypeError(evaluated)
+
+    def __str__(self):
+        return '~%s' % self.children[0]
+
+    __repr__ = __str__
+
+    def distribute_inwards(self):
+        child = self.children[0]
+        if isinstance(child, bool):
+            return not child
+        elif isinstance(child, Not):
+            # Simplify double negation.
+            return child.children[0]
+        elif isinstance(child, Or):
+            # Instance of de Morgan's Law: ~(x | y) === (~x & ~y)
+            return And(*(Not(descendant).distribute_inwards() for descendant in child.children))
+        elif isinstance(child, And):
+            # Instance of de Morgan's Law: ~(x & y) === (~x | ~y)
+            return Or(*(Not(descendant).distribute_inwards() for descendant in child.children))
+        else:
+            return self
---
+++ pyreasoner/tests/__init__.py
@@ -0,0 +1,24 @@
+from unittest import TestCase
+
+from ..expressions import is_conjunctive_normal_form
+from ..expressions import convert_to_conjunctive_normal_form
+
+a, b, c, d, e = variables('a b c d e')
+
+cnf_expressions = (
+    ~a & (b | c),
+    (a | b) & (~b | c | ~d) & (d | e),
+    a | b,
+    a & b
+)
+
+
+class TestConjunctiveNormalForm(TestCase):
+    def test_cnf_expressions(self):
+        for cnf_expression in cnf_expressions:
+            self.assertTrue(
+                is_conjunctive_normal_form(cnf_expression),
+                cnf_expression)
+            self.assertEqual(
+                convert_to_conjunctive_normal_form(cnf_expression),
+                cnf_expression)
